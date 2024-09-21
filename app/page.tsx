4ac3bb950017e5190a39a9fee55db84b110f0ebc
unknown <jegq.dev.projects@gmail.com>
2024-09-21 10:26:00 -0400

    Consultar el workspace más reciente y redirigir si no tiene workspaces

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-const Home = () => {
-  return (
-    <main className="">
-      <h1>Slack clone</h1>
-    </main>
-  );
+import { redirect } from "next/navigation";
+import { getAllUserWorkspaces } from "@/utils/getUserData";
+
+const Home = async () => {
+  // Consultar el útimo workspace del usuario
+  const workspaces = await getAllUserWorkspaces(true);
+
+  if (workspaces.length === 0) {
+    return redirect("workspace/create");
+  }
+
+  return redirect(`/workspace/${workspaces[0].id}`);
 }
 
 export default Home;
---
+++ utils/getUserData.ts
@@ -0,0 +1,74 @@
+import { notFound, redirect } from "next/navigation";
+import { supabaseServerClient } from "./supabase/supabaseServerClient";
+import { Workspace } from "@/types/supabase";
+
+
+/** Consultar la data del usuario autentcado */
+export const getUserData = async () => {
+  const supabase = supabaseServerClient();
+
+  const {data: {user}} = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  const {data, error} = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", user.id);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data[0];
+}
+
+
+/** Consultar todos los workspaces del usuario autenticado o el último al que se unió */
+export const getAllUserWorkspaces = async (latest: boolean = false) => {
+  const supabase = supabaseServerClient();
+
+  const {data: {user}} = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  const query = supabase
+    .from("members_workspaces")
+    .select("workspaces(*)")
+    .eq("user_id", user.id)
+    .order("created_at", {ascending: false});
+
+  // Retornar un solo workspace si se solicita sólo el mas reciente
+  if (latest) {
+    query.limit(1);
+  }
+
+  const {data, error} = await query;
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!data) {
+    return [];
+  }
+
+  // Convertir la data de los workspaces en un array de objetos workspace
+  const reduced = data.reduce((acc: Workspace[], curr) => {
+    if (curr.workspaces) {
+      return acc.concat(curr.workspaces)
+    }
+
+    return acc;
+  }, []);
+  
+  return reduced;
+}
