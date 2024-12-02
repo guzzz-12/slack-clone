98ac7a8fb26d7513212c0c57b7e860e627ee786a
unknown <jegq.dev.projects@gmail.com>
2024-12-02 11:10:02 -0400

    State global de la data de los workspaces

---
+++ hooks/useWorkspace.ts
@@ -0,0 +1,22 @@
+import { Workspace, WorkspaceWithMembers } from "@/types/supabase";
+import {create} from "zustand";
+
+interface WorkspaceState {
+  currentWorkspace: WorkspaceWithMembers | null;
+  userWorkspaces: Workspace[];
+  loadingWorkspaces: boolean;
+  setCurrentWorkspace: (workspace: WorkspaceWithMembers | null) => void;
+  setUserWorkspaces: (workspaces: Workspace[]) => void;
+  setLoadingWorkspaces: (loading: boolean) => void;
+}
+
+// Hook para manejar el estado global del workspace
+export const useWorkspace = create<WorkspaceState>()((set) => ({
+  currentWorkspace: null,
+  currentWorkspaceMembers: [],
+  userWorkspaces: [],
+  loadingWorkspaces: true,
+  setCurrentWorkspace: (workspace) => set({currentWorkspace: workspace}),
+  setUserWorkspaces: (workspaces) => set({userWorkspaces: workspaces}),
+  setLoadingWorkspaces: (loading) => set({loadingWorkspaces: loading})
+}));
