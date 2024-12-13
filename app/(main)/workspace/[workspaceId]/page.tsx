b23f45c55fa266aab3d0a6601ae6f364e456f7d9
unknown <jegq.dev.projects@gmail.com>
2024-12-12 20:32:03 -0400

    Página del workspace seleccionado (sin contenido)

---
+++ app/(main)/workspace/[workspaceId]/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useWorkspace } from "@/hooks/useWorkspace";
+import { WorkspaceWithMembers } from "@/types/supabase";
+
+interface Props {
+  params: {
+    workspaceId: string;
+  }
+}
+
+const WorkspaceDetailPage = ({params}: Props) => {
+  const {workspaceId} = params;
+
+  const {setLoadingWorkspaces, setCurrentWorkspace} = useWorkspace();
+
+  // Consultar el workspace y sus miembros
+  useEffect(() => {
+    const fetchWorkspace = async () => {
+      try {
+        setLoadingWorkspaces(true);
+
+        const res = await axios<WorkspaceWithMembers>(`/api/workspace/${workspaceId}`);
+
+        setCurrentWorkspace({
+          workspaceData: res.data.workspaceData,
+          workspaceMembers: res.data.workspaceMembers
+        });
+
+      } catch (error: any) {
+        toast.error(error.message);
+
+      } finally {
+        setLoadingWorkspaces(false);
+      }
+    }
+
+    // Almacenar la ID del workspace seleccionado en el localStorage
+    localStorage.setItem("selectedWorkspaceId", workspaceId);
+
+    fetchWorkspace();
+  }, [workspaceId]);
+
+  return (
+    <main className="flex-grow p-4 rounded-r-lg bg-neutral-900 overflow-y-auto scrollbar-thin">
+      <section className="w-full rounded-r-lg">
+        Workspace Detail Page
+      </section>
+    </main>
+  )
+}
+
+export default WorkspaceDetailPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,7 +60,10 @@ const config: Config = {
       }
     }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwind-scrollbar"),
+  ],
 };
 
 export default config;
