c0b258d6b75e855dda46b636a57e8ac3949bb5f2
unknown <jegq.dev.projects@gmail.com>
2024-12-20 19:15:00 -0400

    Cambio de estilos de la ventana principal de workspace cuando no hay channel seleccionado

--- app/(main)/workspace/[workspaceId]/page.tsx
+++ app/(main)/workspace/[workspaceId]/page.tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation";
 import axios, { AxiosError } from "axios";
 import toast from "react-hot-toast";
 import { FaSlackHash } from "react-icons/fa";
-import { PiHandWavingFill } from "react-icons/pi";
 import { CgSpinner } from "react-icons/cg";
 import { useWorkspace } from "@/hooks/useWorkspace";
 import { Workspace, WorkspaceWithMembers } from "@/types/supabase";
@@ -70,20 +69,16 @@ const WorkspaceDetailPage = ({params}: Props) => {
 
       {!loadingWorkspaces && (
         <>
-          <div className="flex justify-center items-center gap-1 w-full mb-3">
-            <PiHandWavingFill className="w-10 h-10" /> 
-
-            <h1 className="text-4xl text-center font-normal">
-              Welcome to <span className="font-mono">#{(currentWorkspace.workspaceData.name).replace(" ", "")}</span>
-            </h1>
-          </div>
+          <h1 className="mb-2 text-center text-3xl font-normal">
+            Welcome to <span className="font-mono">#{(currentWorkspace.workspaceData.name).replace(" ", "")}</span>
+          </h1>
 
           <section className="flex flex-col justify-center items-center gap-3 w-full">
-            <h2 className="text-xl text-center">
-              Select a channel or private conversation
+            <h2 className="text-lg text-center text-neutral-300">
+              Select a channel or a private conversation
             </h2>
 
-            <FaSlackHash className="w-32 h-32 opacity-20"/>
+            <FaSlackHash className="w-32 h-32 text-neutral-600"/>
           </section>
         </>
       )}
