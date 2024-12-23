7ec359a5b91fea05f0cdf0d6b15cdca3a2473c17
unknown <jegq.dev.projects@gmail.com>
2024-12-22 20:53:02 -0400

    Cliente server-side de supabase para implementación en pages router

---
+++ utils/supabasePagesRouter/supabaseServerClientPages.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createServerClient, serializeCookieHeader } from "@supabase/ssr";
+import { Database } from "@/types/supabase";
+
+// Cliente server-side de supabase para el pages-router
+export const supabaseServerClientPages = (req: NextApiRequest, res: NextApiResponse) => {
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        detectSessionInUrl: true,
+        flowType: "pkce",
+      },
+      cookies: {
+        getAll() {
+          return Object.keys(req.cookies).map((name) => ({ name, value: req.cookies[name] || "" }))
+        },
+        setAll(cookiesToSet) {
+          res.setHeader(
+            "Set-Cookie",
+            cookiesToSet.map(({ name, value, options }) =>
+              serializeCookieHeader(name, value, options)
+            )
+          )
+        },
+      },
+    }
+  );
+
+  return supabase
+}
