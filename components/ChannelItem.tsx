c26ec22d5313a5b059d1f9e88174948bc0210701
unknown <jegq.dev.projects@gmail.com>
2025-01-01 20:08:43 -0400

    Item del channel

---
+++ components/ChannelItem.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { GoHash } from "react-icons/go";
+import Typography from "./Typography";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import { Channel, MessageWithSender, } from "@/types/supabase"
+import { cn } from "@/lib/utils";
+
+interface Props {
+  channel: Channel;
+  currentChannelId: string;
+  unreadMessages: MessageWithSender[];
+}
+
+const ChannelItem = ({currentChannelId, channel, unreadMessages}: Props) => {
+  const unreadCount = unreadMessages.filter((m) => m.channel_id === channel.id).length;
+
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={250}>
+        <TooltipTrigger className="flex justify-start w-full">
+          <Link
+            className="w-full overflow-hidden"
+            href={`/workspace/${channel.workspace_id}/channel/${channel.id}`}
+          >
+            <div className={cn("flex justify-start items-center gap-1 px-2 py-1 rounded-sm bg-transparent cursor-pointer hover:bg-neutral-700 transition-colors", channel.id === currentChannelId && "bg-neutral-700")}>
+              <GoHash className="flex-shrink-0" />
+              <Typography
+                className="w-full flex-grow text-sm text-left truncate"
+                variant="p"
+                text={channel.name}
+              />
+
+              {unreadCount > 0 && (
+                <span className="flex justify-center items-center min-w-[24px] h-[24px] p-1.5 flex-shrink-0 text-[12px] font-semibold text-white rounded-full bg-primary-dark">
+                  {unreadCount > 99 ? "99+" : unreadCount}
+                </span>
+              )}
+                
+            </div>
+          </Link>
+        </TooltipTrigger>
+
+        <TooltipContent side="right">
+          {channel.name}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export default ChannelItem
