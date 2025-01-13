0775cbb1f34cfe08da59f1dcf753e7e56cfca02e
unknown <jegq.dev.projects@gmail.com>
2025-01-13 17:30:58 -0400

    Endpoint para generar el token de livekit

---
+++ app/api/livekit/token/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { AccessToken } from "livekit-server-sdk";
+
+// Route handler para generar el token de LiveKit
+export async function GET(req: NextRequest) {
+  try {
+    const room = req.nextUrl.searchParams.get("room");
+    const username = req.nextUrl.searchParams.get("username");
+  
+    if (!room) {
+      return NextResponse.json({ error: "Missing 'room' query parameter" }, { status: 400 });
+    } else if (!username) {
+      return NextResponse.json({ error: "Missing 'username' query parameter" }, { status: 400 });
+    }
+  
+    const apiKey = process.env.LIVEKIT_API_KEY;
+    const apiSecret = process.env.LIVEKIT_API_SECRET;
+    const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+  
+    if (!apiKey || !apiSecret || !wsUrl) {
+      return NextResponse.json({ error: "Server misconfigured" }, { status: 500 });
+    }
+  
+    const at = new AccessToken(apiKey, apiSecret, { identity: username });
+  
+    at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+  
+    return NextResponse.json({ token: await at.toJwt() });
+    
+  } catch (error: any) {
+    console.log(error.message);
+
+    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+  }
+}
