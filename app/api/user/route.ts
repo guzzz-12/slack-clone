abbca5e5b9ec3c5ad6419a57a5eafcaae7468ba1
unknown <jegq.dev.projects@gmail.com>
2025-02-02 12:29:01 -0400

    Subir el avatar al bucket si el usuario cambió el avatar

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,15 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { redirect } from "next/navigation";
+import { v4 } from "uuid";
 import { isPostgresError } from "@/utils/constants";
 import { supabaseServerClient } from "@/utils/supabase/supabaseServerClient";
+import { b2Client } from "@/utils/backblaze";
+import { UploadResponseData, UploadUrlData } from "@/types/backblaze";
 
 export async function PATCH(req: NextRequest) {
   try {
-    const {name} = await req.json() as {name: string};
-
-    if (!name || name.trim() === "" || name.length < 3) {
-      return NextResponse.json({message: "Your name is required"}, {status: 400});
-    }
+    const formData = await req.formData();
+    const name = formData.get("name") as string;
+    const avatar = formData.get("avatar") as File | undefined;
+    let avatarUrl: string | null = "";
+    let avatarName: string | null = "";
+    let avatarId: string | null = "";
 
     const supabase = supabaseServerClient();
 
@@ -19,9 +23,64 @@ export async function PATCH(req: NextRequest) {
       return redirect("/signin");
     }
 
+    // Consultar el perfil del usuario
+    const {data: userProfile, error: userProfileError} = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", currentUser.user.id)
+      .single();
+
+    if (userProfileError) {
+      throw userProfileError;
+    }
+
+    // Verificar si se ha enviado una imagen y subirla
+    // dal bucket luego de eliminar la anterior si existe
+    if (avatar) {
+      // Convertir la imagen en buffer para que sea compatible con backblaze
+      const buffer = Buffer.from(await avatar.arrayBuffer());
+  
+      // Autorizar backblaze
+      await b2Client.authorize();
+
+      // Eliminar la imagen anterior del bucket si existe
+      if (userProfile.avatar_id) {
+        await b2Client.deleteFileVersion({
+          fileId: userProfile.avatar_id as string,
+          fileName: userProfile.avatar_name as string
+        });
+      }
+
+      // Generar la url de subida del archivo
+      const uploadUrl = await b2Client.getUploadUrl({
+        bucketId: process.env.BACKBLAZE_BUCKET_ID as string,
+      });
+  
+      // Data de la url de subida del archivo al bucket
+      const urlData = uploadUrl.data as UploadUrlData;
+
+      // Subir la imagen al bucket
+      const uploadRes = await b2Client.uploadFile({
+        uploadAuthToken: urlData.authorizationToken,
+        uploadUrl: urlData.uploadUrl,
+        data: buffer,
+        fileName: `user-avatars/${currentUser.user.id}/${v4()}.webp`,
+      });
+
+      // Data de la imagen subida al bucket
+      const uploadData = uploadRes.data as UploadResponseData;
+
+      avatarUrl = `${process.env.BACKBLAZE_BUCKET_URL}/${uploadData.fileName}`;
+      avatarName = uploadRes.data.fileName;
+      avatarId = uploadRes.data.fileId;
+    }
+
     const {data: updatedUserData, error} = await supabase
     .from("users")
-    .update({name})
+    .update({
+      name,
+      ...(avatarName && avatarUrl && avatarId ? {avatar_url: avatarUrl, avatar_name: avatarName, avatar_id: avatarId} : {}),
+    })
     .eq("id", currentUser.user.id)
     .select("*")
     .single();
@@ -40,7 +99,7 @@ export async function PATCH(req: NextRequest) {
       console.log(`Error PostgreSQL actualizando perfil del usuario: ${message}, code: ${code}`);
 
     } else {
-      console.log(`Error actualizando perfil del usuario: ${message}`);      
+      console.log(`Error actualizando perfil del usuario: ${message}`, error);      
     }
 
     return NextResponse.json({message: "Internal server error"}, {status: 500});
