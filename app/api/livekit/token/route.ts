c59be91a933213f5eb0c5cc303e32d7c33316f4e
unknown <jegq.dev.projects@gmail.com>
2025-02-03 19:56:58 -0400

    Emitir evento de videollamada privada al otro usuario

--- app/api/livekit/token/route.ts
+++ app/api/livekit/token/route.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { AccessToken } from "livekit-server-sdk";
+import { pusher } from "@/utils/pusher";
 
 // Route handler para generar el token de LiveKit
 export async function GET(req: NextRequest) {
   try {
-    const room = req.nextUrl.searchParams.get("room");
-    const username = req.nextUrl.searchParams.get("username");
+    const searchParams = new URL(req.url).searchParams;
+    const room = searchParams.get("room");
+    const username = searchParams.get("username");
+    const workspaceId = searchParams.get("workspace_id");
+    const callerId = searchParams.get("caller_id");
+    const callType = searchParams.get("call_type") as "channel" | "private";
   
     if (!room) {
       return NextResponse.json({ error: "Missing 'room' query parameter" }, { status: 400 });
@@ -24,6 +29,10 @@ export async function GET(req: NextRequest) {
     const at = new AccessToken(apiKey, apiSecret, { identity: username });
   
     at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+
+    if (callType === "private") {
+      pusher.trigger(`videocall-${callerId}-${workspaceId}`, "incoming-call", {callerId});
+    }
   
     return NextResponse.json({ token: await at.toJwt() });
     
