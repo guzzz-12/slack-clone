1b16722d94fcd1e6ae66b85af0436b64ef3334bb
unknown <jegq.dev.projects@gmail.com>
2025-02-03 20:00:10 -0400

    Escuchar evento de videollamada privada entrante y videollamada privada finalizada

--- components/PrivateChatItem.tsx
+++ components/PrivateChatItem.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { FaVideo } from "react-icons/fa";
 import Typography from "./Typography";
-import { PrivateMessageWithSender, WorkspaceMember } from "@/types/supabase";
-import { cn } from "@/lib/utils";
 import { pusherClient } from "@/utils/pusherClientSide";
+import { cn } from "@/lib/utils";
+import { PrivateMessageWithSender, WorkspaceMember } from "@/types/supabase";
+import { useMessages } from "@/hooks/useMessages";
+import { combineUuid } from "@/utils/constants";
+import { useUser } from "@/hooks/useUser";
 
 type Params = {
   workspaceId: string;
@@ -20,10 +24,44 @@ interface Props {
 const PrivateChatItem = ({workspaceId, member, unreadMessages}: Props) => {
   const params = useParams<Params>();
 
+  const [incomingVideoCall, setIncomingVideoCall] = useState(false);
   const [isAway, setIsAway] = useState(() => member.is_away);
 
   const unreadCount = unreadMessages.filter((m) => m.sender.id === member.id).length;
   const isItemActive = params.userId === member.id && params.workspaceId === workspaceId;
+
+  const {user: currentUser} = useUser();
+  const {setCallerId, setVideoCallType} = useMessages();
+
+
+  // Escuchar evento de video llamada entrante y
+  // evento de finalización de video llamada
+  useEffect(() => {
+    if (!member || !currentUser) return;
+
+    // Generar el callerId a partir de las ids de los usuarios de la conversación
+    const combinedUserIds = combineUuid(currentUser.id, member.id);
+    
+    const channel = pusherClient.subscribe(`videocall-${combinedUserIds}-${workspaceId}`);
+    
+    channel.bind("incoming-call", ({callerId}: {callerId: string}) => {
+      console.log({incoming_call: callerId});
+      setIncomingVideoCall(true);
+      setCallerId(callerId);
+      setVideoCallType("private");
+    });
+    
+    channel.bind("call-ended", ({callerId}: {callerId: string}) => {
+      console.log({videcall_ended: callerId});
+      setCallerId(null);
+      setIncomingVideoCall(false);
+      setVideoCallType(null);
+    });
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [workspaceId, member, currentUser]);
 
 
   // Escuchar el evento de cambio de presencia del usuario
@@ -43,10 +81,10 @@ const PrivateChatItem = ({workspaceId, member, unreadMessages}: Props) => {
 
   return (
     <Link
-      className="block w-full"
+      className="block w-full overflow-hidden"
       href={`/workspace/${workspaceId}/private-chat/${member.id}`}
     >
-      <div className={cn("flex justify-start items-center gap-1 w-full rounded-sm bg-neutral-700/30 cursor-pointer hover:bg-neutral-600 transition-colors", isItemActive && "bg-neutral-950")}>
+      <div className={cn("flex justify-start items-center gap-1 w-full pr-2 rounded-sm bg-neutral-700/30 cursor-pointer hover:bg-neutral-600 transition-colors", isItemActive && "bg-neutral-950")}>
         <div className="flex justify-start items-center gap-2 flex-grow p-2 overflow-hidden">
           <div className="relative w-[24px] h-[24px] rounded-full flex-shrink-0">
             <img
@@ -68,6 +106,13 @@ const PrivateChatItem = ({workspaceId, member, unreadMessages}: Props) => {
           <span className="flex justify-center items-center min-w-[24px] h-[24px] p-1.5 flex-shrink-0 text-[12px] font-semibold text-white rounded-full bg-primary-dark">
             {unreadCount > 99 ? "99+" : unreadCount}
           </span>
+        )}
+
+        {incomingVideoCall && (
+          <div className="relative flex justify-center items-center flex-shrink-0 rounded-full">
+            <div className="absolute inline-flex h-5 w-5 animate-ping rounded-full bg-green-500 opacity-75 z-10"/>
+            <FaVideo className="relative block w-4 h-4 flex-shrink-0 text-white z-20" />
+          </div>
         )}
       </div>
     </Link>
