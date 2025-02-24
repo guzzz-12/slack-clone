ab511ef169c20076a163b4bcc0c393f0d107580d
unknown <jegq.dev.projects@gmail.com>
2025-02-24 12:16:35 -0400

    Enviar correo al administrador al procesar el cron job de vercel

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,8 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
+import sendgrid from "@sendgrid/mail";
 import { supabaseServerClient } from "@/utils/supabase/supabaseServerClient";
+import { cronJobEmailTemplate } from "@/utils/cronJobEmailTemplate";
 
 export const dynamic = "force-dynamic";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
 
 // Endpoint del cron job de vercel para evitar que el proyecto de supabase se deshabilite por inactividad
 export async function GET(_req: NextRequest) {
@@ -10,7 +14,7 @@ export async function GET(_req: NextRequest) {
     const headerList = headers();
 
     if (headerList.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
-      return NextResponse.json({message: "Unauthorized cron request"}, {status: 401});
+      return NextResponse.json({ message: "Unauthorized cron request" }, { status: 401 });
     }
 
     const supabase = supabaseServerClient();
@@ -22,12 +26,26 @@ export async function GET(_req: NextRequest) {
       throw error;
     }
 
-    console.log({CRON_JOB_DATA: {activeUsers: data}});
+    console.log({ CRON_JOB_DATA: { activeUsers: data } });
 
-    return NextResponse.json({CRON_JOB_DATA: {activeUsers: data}});
-    
+    // Opciones del correo a enviar
+    const mailContent = {
+      to: process.env.SENDGRID_EMAIL as string,
+      from: {
+        name: "TeamFlow App",
+        email: process.env.SENDGRID_EMAIL as string
+      },
+      subject: "Resultados del cron job de vercel",
+      html: cronJobEmailTemplate({ activeUsers: data })
+    };
+
+    // Enviar el email
+    await sendgrid.send(mailContent);
+
+    return NextResponse.json({ CRON_JOB_DATA: { activeUsers: data } });
+
   } catch (error: any) {
     console.log(`Error consultando endpoint del cron job de vercel:`, error.message);
-    return NextResponse.json({message: "Internal server error"}, {status: 500});
+    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
   }
 }
